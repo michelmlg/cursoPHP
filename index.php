77ca23fc848e4285f5cf1906f1497cc2f71eb3f8
michelmlg <147891522+michelmlg@users.noreply.github.com>
2024-08-07 01:09:00 -0300

    desafio_string

--- index.php
+++ index.php
@@ -77,6 +77,11 @@
                                 Tipo String
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=desafio_string">
+                                Desafio String
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
--- tipos/desafio_string.php
+++ tipos/desafio_string.php
@@ -1,7 +1,31 @@
-<div class="titulo">Desefio String</div>
+<div class="titulo">Desafio String</div>
 <?php
 
+    // !AbcaBcabc
 
+    // Enunciado:
+    // Avaliando os métodos da documentação da string, qual método que a posição do texto 'abc' na string '!AbcaBcabc' retorne 1?
 
+    $string = "!AbcaBcabc";
+    $substring = "abc";
 
+    // Tentativa 1
+
+    // function mudaPosicao($string){
+    //     $strDepois = substr($string, 7);
+    //     if($strDepois == "abc"){
+    //         return 1;
+    //     }else{
+    //         return 0;
+    //     }
+    // }
+    // mudaPosicao($string);
+
+    // Tentativa 2
+
+    echo strpos($string,"abc") . '<br>';
+
+    echo stripos($string, "abc") . '<br>';
+
+    echo strpos(strtolower("!AbcaBcabc"), 'abc') . '<br>';
 ?>
